b9288ae5a7596e21a986a5245486dabee188f52e
Erick Vieira <erick@linedata.com.br>
2020-09-25 12:50:33 -0300

    Lista de usuários

--- src/Componentes/Usuarios/Usuarios.js
+++ src/Componentes/Usuarios/Usuarios.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Usuarios.css';
 import Menu from '../Menu/Menu'
+import axios from 'axios';
 
-import { Container, Row, Col, Table, Form, Button } from 'react-bootstrap';
-import Logo from '../../Imagens/logo.jpg';
+import { Container, Row, Col, Table, Form, Button, Modal } from 'react-bootstrap';
+import Usuario from '../Modal/Usuario/Usuario';
 
-const url = "http://localhost:1100/api/usuarios/";
+const url = "http://localhost:1100/api/";
+
+const states = {
+    usuarios: [
+
+    ]
+}
+
 
 export default class Usuarios extends React.Component {
+    state = {...states};
 
-    render() {
+    componentWillMount(){
+        axios["get"](url + `usuario/obtemTodos`).then(resp => {
+            // state.usuarios = resp.data;
+            this.setState({usuarios: resp.data});
+        });
+    }
+
+    render(){
         return (
             <div>
                 <Menu />
@@ -29,11 +45,10 @@ export default class Usuarios extends React.Component {
                             </Row>
                         </Col>
                         <Col md={1}>
-                            <Button type="submit" className="mb-2">Adicionar</Button>
+                            <Usuario />
                         </Col>
                     </Row>
-
-
+    
                     <Table striped bordered hover>
                         <thead>
                             <tr>
@@ -41,10 +56,26 @@ export default class Usuarios extends React.Component {
                                 <th>Nome</th>
                                 <th>CPF</th>
                                 <th>Cargo</th>
+                                <th>Ações</th>
                             </tr>
                         </thead>
                         <tbody>
-
+                            {
+                                this.state.usuarios.map(function(usuario){
+                                    return(
+                                        <tr key={usuario.id}>
+                                            <td>{usuario.id}</td>
+                                            <td>{usuario.nome}</td>
+                                            <td>{usuario.cpf}</td>
+                                            <td>{usuario.cargo.descricao}</td>
+                                            <td className='botoes'>
+                                                <Button variant="warning">Editar</Button>
+                                                <Button variant="danger">Remover</Button>
+                                            </td>
+                                        </tr>
+                                    );
+                                })
+                            }
                         </tbody>
                     </Table>
                 </Container>
